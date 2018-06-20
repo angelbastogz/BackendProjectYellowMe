ceb9afcda7fb9ee501647d306622a9c0d3ad5dd3
= <=>
2018-06-19 22:46:20 -0500

    add validations in model url

--- app/controllers/api/v1/urls_controller.rb
+++ app/controllers/api/v1/urls_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::UrlsController < ApplicationController
-include UrlsHelper
+  include UrlsHelper
 
+  #POST api/v1/generate
   def generate_shorter_url
     url_original = params[:url]
-    puts url_original
     path = get_path
 
     #if the parameter is null render error
@@ -18,7 +18,6 @@ include UrlsHelper
       if !Url.exists?(original: url_original)
         url = Url.new
         url.original = url_original
-        url.generated_code = generate_code
 
         if url.save
           render json: {status: :ok, generated_url: path+url.generated_code}
@@ -30,16 +29,6 @@ include UrlsHelper
         render json: {status: :ok, generated_url: path+url.generated_code}
       end
     end
-
   end
 
-  private
-
-  #returns a randomly generated code
-  def generate_code
-    number = 6
-
-    charset = Array('A'..'Z') + Array('a'..'z') + Array('0'..'9')
-    Array.new(number) { charset.sample }.join
-  end
 end
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,21 @@
 class Url < ApplicationRecord
+  validates :original, :generated_code, presence: true
+
+  before_validation :generate_unique_code
+
+  def generate_unique_code
+    begin
+      generated_code = generate_code
+    end while Url.exists?(generated_code: generated_code)
+
+    self.generated_code = generated_code
+  end
+
+  #returns a randomly generated code
+  def generate_code
+    number = 6
+
+    charset = Array('A'..'Z') + Array('a'..'z') + Array('0'..'9')
+    Array.new(number) { charset.sample }.join
+  end
 end
